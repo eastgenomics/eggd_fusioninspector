a1278eaec7c7b5626e59b25c5deb92d9d2b5e5ea
addy81 <adriana.tou@gmail.com>
2020-08-19 19:52:28 +0100

    initial working app taking two fusion lists as input

---
+++ src/fusion_inspector.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+# fusion_inspector 1.0.0
+# Extracts a pair of genes from the genome, creates a mini-contig, 
+# aligns reads to the mini-contig, and extracts the fusion reads as a separate tier for vsiualization
+
+
+# Output each line as it is executed (-x) and don't stop if any non zero exit codes are seen (+e)
+set -x +e
+
+mkdir -p out/fi_outputs/
+
+# Get fusion inspector docker image
+sudo docker pull trinityctat/fusioninspector
+
+# download genome resources and decompress
+dx cat "$genome_lib" | tar zxf -
+
+# download remaining inputs
+dx download "$left_fq" -o R1.fastq.gz
+dx download "$right_fq" -o R2.fastq.gz
+dx download "$known_fusions" -o fusions_list.txt
+dx download "$sr_predictions" -o predicted_fusions.tsv
+
+# download genome resources and decompress
+dx cat "$genome_lib" | tar zxf - 
+
+# Sets genome variable 
+CTAT_GENOME_LIB=/home/GRCh37_gencode_v19_CTAT_lib_Apr032020.plug-n-play/ctat_genome_lib_build_dir
+
+# Prefix for sample naming - to be fixed once workflow is added
+sample=($left_fq_prefix)
+prefix="${sample/'_R1_concat'/''}"
+
+# Extracts the fusion pairs from the predictions file (unfiltered)
+cut -f 1 predicted_fusions.tsv | grep -v '#FusionName' > predicted_fusions.txt
+
+
+# Runs fusion inspector using known_fusions and predicted_fusions files 
+sudo docker run -v `pwd`:/home --rm trinityctat/fusioninspector:latest FusionInspector  \
+       --fusions /home/fusions_list.txt,/home/predicted_fusions.txt \
+       -O /home/out/fi_outputs/${prefix} \
+       --left_fq /home/R1.fastq.gz \
+       --right_fq /home/R2.fastq.gz \
+       --out_prefix ${prefix}\
+       --genome_lib_dir ${CTAT_GENOME_LIB}
+       --vis \
+       --include_Trinity \
+       --examine_coding_effect \
+       --extract_fusion_reads_file ${prefix}.FusionInspector-pe_samples/fusion_reads 
+
+
+# upload all outputs
+dx-upload-all-outputs --parallel
+
+
