e52a3f86f14011759f53f6f32f8252f7a5059251
corbin-chris <chris.corbin2@nhs.net>
2022-11-11 14:28:11 +0000

    Fixing read path formations

--- src/code.sh
+++ src/code.sh
@@ -22,10 +22,13 @@ find ./in/r1_fastqs -type f -name "*R1*" -print0 | xargs -0 -I {} mv {} ./r1_fas
 find ./in/r2_fastqs -type f -name "*R2*" -print0 | xargs -0 -I {} mv {} ./r2_fastqs
 
 # set up 1 or more fastq files in a list
-read_1=$(find ./r1_fastqs/ -type f -name "*" -name "*R1*.fastq*" -printf '%p,' | \
-sed 's/\.\///g' | sed -e 's/^/\/data\//')
-read_2=$(find ./r2_fastqs/ -type f -name "*" -name "*R2*.fastq*" -printf '%p,' | \
-sed 's/\.\///g' | sed -e 's/^/\/data\//')
+# TODO: /data/ only getting added to first read 1 entry at the moment - xarg?
+read_1=$(find ./r1_fastqs/ -type f -name "*" -name "*R1*.fastq*" | \
+sed 's/\.\///g' | sed -e 's/^/\/data\//' | paste -sd, -)
+read_2=$(find ./r2_fastqs/ -type f -name "*" -name "*R2*.fastq*" | \
+sed 's/\.\///g' | sed -e 's/^/\/data\//' | paste -sd, -)
+echo "$read_1"
+echo "$read_2"
 
 # get names of fusion files for Docker
 known_fusions_name=$(find /home/dnanexus/in/known_fusions -type f -printf "%f\n")
@@ -45,6 +48,10 @@ prefix=$(echo "$sr_predictions_name" | cut -d '.' -f 1)
 cut -f 1 /home/dnanexus/in/sr_predictions/"${sr_predictions_name}" \
 | grep -v '#FusionName' > /home/dnanexus/in/sr_predictions/predicted_fusions.txt
 
+# Set up fastq paths for Docker
+left_fastqs="${read_1::-1}"
+right_fastqs="${read_2::-1}"
+
 mark-section "run FusionInspector"
 
 # Runs fusion inspector using known_fusions and predicted_fusions files
@@ -53,8 +60,8 @@ sudo docker run -v "$(pwd)":/data --rm \
        FusionInspector  \
        --fusions /data/in/known_fusions/"${known_fusions_name}",/data/in/sr_predictions/predicted_fusions.txt \
        -O /data/out/fi_outputs \
-       --left_fq "${read_1::-1}" \
-       --right_fq "${read_2::-1}" \
+       --left_fq "${left_fastqs}" \
+       --right_fq "${right_fastqs}" \
        --out_prefix "${prefix}" \
        --genome_lib_dir "/data/${lib_dir}/ctat_genome_lib_build_dir" \
        --vis \
