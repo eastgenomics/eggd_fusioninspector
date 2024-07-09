0061a3918aecbab9e2eb4f76c428869402e9d052
AishaD <AishaDahir75@gmail.com>
2024-07-09 10:03:18 +0100

    output inspected/missed fusions

--- src/code.sh
+++ src/code.sh
@@ -171,6 +171,12 @@ fi
 mark-section "Running FusionInspector"
 eval "${fusion_ins}"
 
+mark-section "Check what fusion contigs were selected from the BAM file"
+cut -f 1 /home/dnanexus/temp_out/*.consolidated.bam.frag_coords | sort | uniq > ${prefix}_fusion_contigs_inspected.txt
+
+cat ${known_fusions} /data/sr_predictions/${sr_predictions_name} > fusion_rescue_list.txt
+diff <(cut -f 1 ${prefix}_fusion_contigs_inspected.txts | sort | uniq) \
+<(sort fusion_rescue_list.txt | uniq) | grep ">" | cut -d " " -f 2 > ${prefix}_missed_fusion_contigs.txt
 
 mark-section "Move results files to their output directories"
 
