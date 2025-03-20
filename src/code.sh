7d08bf04c378c8ddb4c9ae894f9cbeb224a4b495
AishaD <AishaDahir75@gmail.com>
2025-03-20 10:33:51 +0000

    coderabbit suggestions

--- src/code.sh
+++ src/code.sh
@@ -341,10 +341,10 @@ main() {
        mark-section "Downloading files from subjobs to the parent job"
        mkdir subjob_output
 
-       # wait 60 seconds before trying to download all files as some files
+       # wait 30 seconds before trying to download all files as some files
        # can still be in an open state due to parallel uploading still
        # commencing even after subjobs have completed successful
-       echo "Waiting 60 seconds to ensure all files are hopefully in a closed state"
+       echo "Waiting 30 seconds to ensure all files are hopefully in a closed state"
        sleep 30
 
        for fusion in "${fusion_lists[@]}"; do
@@ -355,7 +355,7 @@ main() {
               sub_job_tars=$(dx find data --json --name $tar_name --path "$DX_WORKSPACE_ID:/FI_outputs" | jq -r '.[].id')
               # check the file state again
               file_state=$(dx describe $sub_job_tars --json | jq -r '.state')
-              if [ $file_state = "open" ]; then
+              if [ "$file_state" = "open" ]; then
                      sleep 30
               fi
               # try to put all subjob_output folder
