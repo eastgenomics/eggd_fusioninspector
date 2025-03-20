17110aa20c68af6ba71b64cd218fd718414c28c4
AishaD <AishaDahir75@gmail.com>
2025-03-20 10:01:25 +0000

    handle open state files

--- src/code.sh
+++ src/code.sh
@@ -341,6 +341,12 @@ main() {
        mark-section "Downloading files from subjobs to the parent job"
        mkdir subjob_output
 
+       # wait 60 seconds before trying to download all files as some files
+       # can still be in an open state due to parallel uploading still
+       # commencing even after subjobs have completed successful
+       echo "Waiting 60 seconds to ensure all files are hopefully in a closed state"
+       sleep 60
+
        for fusion in "${fusion_lists[@]}"; do
 	       echo $fusion
 	       echo ${fusion%.*}
@@ -348,7 +354,12 @@ main() {
               mkdir subjob_output/inputs_${fusion%.*}
               # download the subjob files currently living in the container
               sub_job_tars=$(dx find data --json --name $tar_name --path "$DX_WORKSPACE_ID:/FI_outputs" | jq -r '.[].id')
-              # try to put all subjob_output folder 
+              # check the file state again
+              file_state=$(dx describe $sub_job_tars --json | jq -r '.state')
+              if [ $file_state = "open" ]; then
+                     sleep 60
+              fi
+              # try to put all subjob_output folder
               echo "$sub_job_tars" | xargs -P $IO_PROCESSES -n1 -I{} sh -c "dx cat $DX_WORKSPACE_ID:{} | tar xf - -C subjob_output/inputs_${fusion%.*}"
        done
 
